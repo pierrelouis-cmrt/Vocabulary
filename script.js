e36a0d3ce14552cfd4b769d36407081ade971615
Josh <113625417+ProjectAdonis@users.noreply.github.com>
2024-02-11 13:58:46 +0100

    progress status + well done screen

---
+++ script.js
@@ -0,0 +1,320 @@
+var categories = {
+  travel: [
+    {
+      word: "a thirst for adventure",
+      translation: "une soif d'aventure",
+    },
+    { word: "get itchy", translation: "avoir la bougeotte" },
+    {
+      word: "contain my excitement",
+      translation: "contenir mon excitation",
+    },
+    { word: "arduous journey", translation: "voyage pénible" },
+    { word: "broke the journey", translation: "interrompre le voyage" },
+    { word: "have a stopover", translation: "faire une escale" },
+    { word: "do the sights", translation: "faire les visites" },
+    {
+      word: "low-cost airline",
+      translation: "compagnie aérienne à bas coût",
+    },
+    {
+      word: "put someone on standby",
+      translation: "mettre quelqu'un en attente",
+    },
+    { word: "go trekking", translation: "faire du trekking" },
+    {
+      word: "off the beaten track",
+      translation: "hors des sentiers battus",
+    },
+    { word: "intrepid explorer", translation: "explorateur intrépide" },
+    { word: "unexplored wilderness", translation: "contrée inexplorée" },
+    { word: "sense of adventure", translation: "sens de l'aventure" },
+    { word: "hopelessly lost", translation: "irrémédiablement perdu" },
+    {
+      word: "send out a search party",
+      translation: "envoyer une équipe de secours",
+    },
+    { word: "sunny spells", translation: "éclaircies" },
+    { word: "boost the spirits", translation: "remonter le moral" },
+    {
+      word: "face severe weather conditions",
+      translation: "affronter des conditions météorologiques difficiles",
+    },
+    {
+      word: "spirits are high",
+      translation: "le moral est au beau fixe",
+    },
+    { word: "sheer epic grandeur", translation: "pure grandeur épique" },
+    {
+      word: "have a special charm",
+      translation: "avoir un charme particulier",
+    },
+    {
+      word: "keep your eyes peeled",
+      translation: "garder les yeux ouverts",
+    },
+    {
+      word: "your budget doesn't quite stretched to",
+      translation: "votre budget ne vous permet pas vraiment de",
+    },
+    {
+      word: "lull you to sleep",
+      translation: "vous bercer pour vous endormir",
+    },
+    { word: "leg of the journey", translation: "étape du voyage" },
+  ],
+  sports: [
+    {
+      word: "do extreme sports",
+      translation: "faire des sports extrêmes",
+    },
+    { word: "acquire a taste for", translation: "prendre goût à" },
+    { word: "keep my balance", translation: "garder mon équilibre" },
+    {
+      word: "get the hang of it",
+      translation: "prendre le coup de main",
+    },
+    { word: "take up the challenge", translation: "relever le défi" },
+    { word: "pretty fit", translation: "assez en forme" },
+    {
+      word: "push myself to the limits",
+      translation: "me pousser à mes limites",
+    },
+    {
+      word: "having a whale of a time",
+      translation: "s'amuser comme des fous",
+    },
+    { word: "jump at the chance", translation: "sauter sur l'occasion" },
+    {
+      word: "summon up the energy",
+      translation: "rassembler son énergie",
+    },
+    { word: "keep in shape", translation: "rester en forme" },
+    {
+      word: "reached fever pitch",
+      translation: "atteindre son paroxysme",
+    },
+    { word: "played brillantly", translation: "jouer brillamment" },
+    { word: "convincing victory", translation: "victoire convaincante" },
+    { word: "the socre stands at", translation: "le score est de" },
+    { word: "latest scores", translation: "derniers résultats" },
+    { word: "take the field", translation: "entrer sur le terrain" },
+    {
+      word: "put up a performance",
+      translation: "faire une performance",
+    },
+    {
+      word: "break through the defence",
+      translation: "percer la défense",
+    },
+    {
+      word: "be awarded a penalty",
+      translation: "se voir accorder un penalty",
+    },
+    { word: "penalty was missed", translation: "penalty raté" },
+    {
+      word: "be given yellow cards",
+      translation: "recevoir des cartons jaunes",
+    },
+    { word: "free kicks", translation: "coups francs" },
+    {
+      word: "scored an own goal",
+      translation: "marquer contre son camp",
+    },
+    { word: "go wild", translation: "se déchaîner" },
+  ],
+  student: [
+    { word: "mental agilty", translation: "agilité mentale" },
+    { word: "gifted children", translation: "enfants doués" },
+    { word: "win a scholarship", translation: "obtenir une bourse" },
+    { word: "attend school", translation: "aller à l'école" },
+    {
+      word: "straight-A student",
+      translation: "élève qui a toujours la meilleure note",
+    },
+    {
+      word: "apply to universities",
+      translation: "postuler aux universités",
+    },
+    { word: "secure a place", translation: "décrocher une place" },
+    {
+      word: "read English litterature",
+      translation: "lire de la littérature anglaise",
+    },
+    { word: "academic year", translation: "année universitaire" },
+    { word: "core subjects", translation: "matières fondamentales" },
+    { word: "set texts", translation: "textes au programme" },
+    {
+      word: "distinguished scholars",
+      translation: "éminents universitaires",
+    },
+    {
+      word: "leading authorities",
+      translation: "autorités de référence",
+    },
+    {
+      word: "graduate from university",
+      translation: "obtenir son diplôme universitaire",
+    },
+    { word: "formal education", translation: "éducation formelle" },
+    {
+      word: "sign up for courses/enroll for courses",
+      translation: "s'inscrire à des cours",
+    },
+    {
+      word: "meet the entry requirements",
+      translation: "remplir les conditions d'admission",
+    },
+    { word: "mature students", translation: "étudiants adultes" },
+    { word: "marked improvement", translation: "nette amélioration" },
+    {
+      word: "my attention wanders",
+      translation: "mon attention se disperse",
+    },
+    { word: "natural talent", translation: "talent naturel" },
+    { word: "full marks", translation: "note maximale" },
+    { word: "room for improvement", translation: "marge de progression" },
+    { word: "learn by heart", translation: "apprendre par coeur" },
+    { word: "thirst for knowledge", translation: "soif de connaissance" },
+    { word: "a quick learner", translation: "un apprenant rapide" },
+    {
+      word: "my concentrations wavers (non steady)",
+      translation: "ma concentration fluctue",
+    },
+    {
+      word: "demonstrate an ability",
+      translation: "faire preuve d'une capacité",
+    },
+    { word: "show initiative", translation: "faire preuve d'initiative" },
+    { word: "proven ability", translation: "capacité avérée" },
+    { word: "playing truant", translation: "faire l'école buissonnière" },
+    {
+      word: "acquire knowledge",
+      translation: "acquérir des connaissances",
+    },
+  ],
+  technology: [
+    { word: "fade out", translation: "s'estomper" },
+    { word: "set up", translation: "installer" },
+    { word: "print off", translation: "imprimer" },
+    { word: "wiped off", translation: "effacer" },
+    { word: "call up", translation: "appeler" },
+    { word: "drop-down", translation: "déroulant" },
+    { word: "type in", translation: "taper" },
+    { word: "wire up", translation: "câbler" },
+    { word: "warm up", translation: "se réchauffer" },
+    { word: "plug into", translation: "brancher sur" },
+    {
+      word: "a (warning) light comes on",
+      translation: "un voyant (d'alerte) s'allume",
+    },
+  ],
+};
+
+var category = "all";
+var index = 0;
+var mode = "word";
+var lastWord = null;
+var originalWords = [];
+var percentageShown = 0;
+var shownWords = [];
+
+function changeCategory() {
+  category = document.getElementById("category").value;
+  index = 0;
+  if (category == "all") {
+    words = [].concat(
+      categories.travel,
+      categories.sports,
+      categories.student,
+      categories.technology
+    );
+  } else {
+    words = categories[category];
+  }
+  originalWords = words.slice();
+  words = originalWords.slice();
+
+  shownWords = [];
+
+  displayWord();
+}
+
+function displayWord() {
+  var word = words[index].word;
+  var translation = words[index].translation;
+
+  var summary = document.getElementById("summary");
+  var details = document.getElementById("details");
+
+  var status = document.getElementById("status");
+  if (mode == "word") {
+    summary.innerText = word;
+    details.innerText = translation;
+    status.innerText = "Visible right now: English";
+  } else {
+    summary.innerText = translation;
+    details.innerText = word;
+    status.innerText = "Visible right now: French";
+  }
+  lastWord = word;
+
+  shownWords.push(words[index].word);
+
+  percentageShown = (shownWords.length / originalWords.length) * 100;
+  var percentage = document.getElementById("percentage");
+  percentage.innerText =
+    "Percentage of words shown from the selected category: " +
+    percentageShown.toFixed(2) +
+    "%";
+}
+
+function switchWord() {
+  if (mode == "word") {
+    mode = "translation";
+  } else {
+    mode = "word";
+  }
+  displayWord();
+}
+
+function shuffleWord() {
+  if (shownWords.length === originalWords.length) {
+    return;
+  }
+
+  words.splice(index, 1);
+
+  for (var i = words.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = words[i];
+    words[i] = words[j];
+    words[j] = temp;
+  }
+
+  index = 0;
+
+  // Add the current word to shownWords
+  shownWords.push(words[index].word);
+
+  // Update the percentage shown
+  percentageShown = (shownWords.length / originalWords.length) * 100;
+  if (shownWords.length === originalWords.length) {
+    percentageShown = 100;
+  }
+
+  var percentage = document.getElementById("percentage");
+  percentage.innerText =
+    "Percentage of words shown from the selected category: " +
+    percentageShown.toFixed(2) +
+    "%";
+
+  // Check if 100% is reached
+  if (percentageShown === 100) {
+    // Show the "Well Done" screen
+    document.getElementById("congratsScreen").style.display = "block";
+  }
+}
+
+window.onload = changeCategory;
+1;
